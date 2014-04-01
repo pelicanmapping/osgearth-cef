707fa597feb5def41bf5af702fca130bced517c5
Jeff Smith <jeffsmith@gmail.com>
2014-04-01 11:45:12 -0400

    Renamed applications to better reflect their purpose: - osgearth_cef_simple is now osgearth_cef - osgearth_cef is now demo_cef

---
+++ applications/demo_cef/main.cpp
@@ -0,0 +1,149 @@
+/* -*-c++-*- */
+/* osgEarth - Dynamic map generation toolkit for OpenSceneGraph
+* Copyright 2008-2013 Pelican Mapping
+* http://osgearth.org
+*
+* osgEarth is free software; you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+#include <osg/Notify>
+#include <osgDB/FileUtils>
+#include <osgViewer/Viewer>
+
+#include <osgEarth/ImageLayer>
+#include <osgEarth/Registry>
+#include <osgEarthUtil/Controls>
+#include <osgEarthDrivers/tms/TMSOptions>
+
+#include <typeinfo>
+
+#include "include/cef_app.h"
+
+#include "CefHelper"
+#include "BrowserClient"
+
+
+#define LC "[osgearth_cef] "
+
+
+using namespace osgEarth::Cef;
+using namespace osgEarth::Util::Controls;
+
+namespace
+{
+    struct MyExecCallback : public ExecuteCallback
+    {
+        CefRefPtr<BrowserClient> _client;
+
+        MyExecCallback(BrowserClient* client) : _client(client) { }
+
+        ExecuteCallback::ReturnVal* execute( int64 query_id, const std::string& command, const JsonArguments &args, CefRefPtr<CefMessageRouterBrowserSide::Callback> persistentCallback )
+        {
+            if (command == "says")
+            {
+              std::string animal = args["animal"];
+              std::string sound = args["sound"];
+
+              return new ExecuteCallback::ReturnVal("The " + animal + " says " + sound);
+            }
+            else if (command == "addTestLayer")
+            {
+                std::string id = args["id"];
+
+                osgEarth::Drivers::TMSOptions tms;
+                tms.url() = "http://readymap.org:8080/readymap/tiles/1.0.0/76/";
+                osgEarth::ImageLayer* layer = new osgEarth::ImageLayer( "Test Layer", tms );
+
+                osg::ref_ptr<osgEarth::MapNode> mapNode= _client->getMapNode(id);
+                if (mapNode.valid())
+                {
+                    mapNode->getMap()->addImageLayer(layer);
+                }
+                else
+                {
+                    return new ExecuteCallback::ReturnVal("Failed to load layer, map node not found.", -1);
+                }
+
+                return new ExecuteCallback::ReturnVal();
+            }
+
+            OE_WARN << LC << "Execute: " << command << std::endl;
+            return 0L;
+        }
+    };
+}
+
+
+
+
+
+int main(int argc, char** argv)
+{
+    osg::ArgumentParser arguments(&argc,argv);
+
+    // Call load to initialize CEF and create the BrowserClient
+    CefRefPtr<BrowserClient> browserClient = CefHelper().load(arguments);
+
+    // Start main loop
+    if (browserClient)
+    {
+        //TEST
+        browserClient->addExecuteCallback(new MyExecCallback(browserClient.get()));
+
+        bool init = false;
+        while (!browserClient->getViewer()->done())
+        {
+            browserClient->getViewer()->frame();
+            CefDoMessageLoopWork();
+
+            // Demo:
+            // Once initialized, add a control to the default canvas
+            if (!init)
+            {
+                // Have to wait until view is created
+                osg::ref_ptr<osgViewer::View> view = browserClient->getMapView("map1");
+                if (view.valid())
+                {
+                    // Get the default ControlCanvas
+                    ControlCanvas* cs = ControlCanvas::get( view.get(), false  );
+
+                    HBox* box = new HBox();
+                    box->setBorderColor( 1, 1, 1, 1 );
+                    box->setBackColor( .6,.5,.4,0.5 );
+                    box->setPadding( 10 );
+                    box->setHorizAlign( Control::ALIGN_LEFT );
+                    box->setVertAlign( Control::ALIGN_BOTTOM );
+
+                    // Add a text label:
+                    LabelControl* label = new LabelControl( "osgEarth Controls Toolkit" );
+                    label->setFont( osgEarth::Registry::instance()->getDefaultFont() );
+                    label->setFontSize( 24.0f );
+                    label->setHorizAlign( Control::ALIGN_LEFT );
+                    label->setMargin( 5 );
+                    box->addControl( label );
+
+                    cs->addControl(box);
+                    
+                    init = true;
+                }
+            }
+        }
+    }
+
+    // Shutdown the CEF processes
+    CefShutdown();
+
+    return 0; 
+}
+
--- applications/osgearth_cef/main.cpp
+++ applications/osgearth_cef/main.cpp
@@ -21,129 +21,18 @@
 #include <osgDB/FileUtils>
 #include <osgViewer/Viewer>
 
-#include <osgEarth/ImageLayer>
-#include <osgEarth/Registry>
-#include <osgEarthUtil/Controls>
-#include <osgEarthDrivers/tms/TMSOptions>
-
-#include <typeinfo>
-
-#include "include/cef_app.h"
-
 #include "CefHelper"
-#include "BrowserClient"
 
 
 #define LC "[osgearth_cef] "
 
 
 using namespace osgEarth::Cef;
-using namespace osgEarth::Util::Controls;
-
-namespace
-{
-    struct MyExecCallback : public ExecuteCallback
-    {
-        CefRefPtr<BrowserClient> _client;
-
-        MyExecCallback(BrowserClient* client) : _client(client) { }
-
-        ExecuteCallback::ReturnVal* execute( int64 query_id, const std::string& command, const JsonArguments &args, CefRefPtr<CefMessageRouterBrowserSide::Callback> persistentCallback )
-        {
-            if (command == "says")
-            {
-              std::string animal = args["animal"];
-              std::string sound = args["sound"];
-
-              return new ExecuteCallback::ReturnVal("The " + animal + " says " + sound);
-            }
-            else if (command == "addTestLayer")
-            {
-                std::string id = args["id"];
-
-                osgEarth::Drivers::TMSOptions tms;
-                tms.url() = "http://readymap.org:8080/readymap/tiles/1.0.0/76/";
-                osgEarth::ImageLayer* layer = new osgEarth::ImageLayer( "Test Layer", tms );
-
-                osg::ref_ptr<osgEarth::MapNode> mapNode= _client->getMapNode(id);
-                if (mapNode.valid())
-                {
-                    mapNode->getMap()->addImageLayer(layer);
-                }
-                else
-                {
-                    return new ExecuteCallback::ReturnVal("Failed to load layer, map node not found.", -1);
-                }
-
-                return new ExecuteCallback::ReturnVal();
-            }
-
-            OE_WARN << LC << "Execute: " << command << std::endl;
-            return 0L;
-        }
-    };
-}
-
-
-
 
 
 int main(int argc, char** argv)
 {
     osg::ArgumentParser arguments(&argc,argv);
 
-    // Call load to initialize CEF and create the BrowserClient
-    CefRefPtr<BrowserClient> browserClient = CefHelper().load(arguments);
-
-    // Start main loop
-    if (browserClient)
-    {
-        //TEST
-        browserClient->addExecuteCallback(new MyExecCallback(browserClient.get()));
-
-        bool init = false;
-        while (!browserClient->getViewer()->done())
-        {
-            browserClient->getViewer()->frame();
-            CefDoMessageLoopWork();
-
-            // Demo:
-            // Once initialized, add a control to the default canvas
-            if (!init)
-            {
-                // Have to wait until view is created
-                osg::ref_ptr<osgViewer::View> view = browserClient->getMapView("map1");
-                if (view.valid())
-                {
-                    // Get the default ControlCanvas
-                    ControlCanvas* cs = ControlCanvas::get( view.get(), false  );
-
-                    HBox* box = new HBox();
-                    box->setBorderColor( 1, 1, 1, 1 );
-                    box->setBackColor( .6,.5,.4,0.5 );
-                    box->setPadding( 10 );
-                    box->setHorizAlign( Control::ALIGN_LEFT );
-                    box->setVertAlign( Control::ALIGN_BOTTOM );
-
-                    // Add a text label:
-                    LabelControl* label = new LabelControl( "osgEarth Controls Toolkit" );
-                    label->setFont( osgEarth::Registry::instance()->getDefaultFont() );
-                    label->setFontSize( 24.0f );
-                    label->setHorizAlign( Control::ALIGN_LEFT );
-                    label->setMargin( 5 );
-                    box->addControl( label );
-
-                    cs->addControl(box);
-                    
-                    init = true;
-                }
-            }
-        }
-    }
-
-    // Shutdown the CEF processes
-    CefShutdown();
-
-    return 0; 
+    return CefHelper().run(arguments);
 }
-
